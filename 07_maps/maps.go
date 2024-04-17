bd48c13b0ada434ace1db6f8ebd07736c6fc7032
Nesrux <joaomarcosdevs@gmail.com>
2024-04-17 18:33:26 -0300

    maps

--- 07_maps/maps.go
+++ 07_maps/maps.go
@@ -1,11 +1,22 @@
 package maps
 
+import "errors"
+
+var ErrNaoEncontrado = errors.New("não foi possivel encontrar a palavra que você porcurou")
+
 type Dicionario map[string]string
 
 func Busca(dicionario map[string]string, palavra string) string {
 	return dicionario[palavra]
 }
 
-func (d Dicionario) Busca(palavra string) string {
-	return d[palavra]
+func (d Dicionario) Busca(palavra string) (string, error) {
+	definicao, exist := d[palavra]
+	if !exist {
+		return "", ErrNaoEncontrado
+	}
+	return definicao, nil
+}
+func (d Dicionario) Adiciona(palavra, definicao string) {
+	d[palavra] = definicao
 }
--- 07_maps/maps_test.go
+++ 07_maps/maps_test.go
@@ -2,16 +2,46 @@ package maps
 
 import "testing"
 
+func Test_Busca(t *testing.T) {
+	dicionario := Dicionario{"Test": "Isso é apenas um teste"}
+	t.Run("Palavra conhecida", func(t *testing.T) {
+		res, _ := dicionario.Busca("Test")
+		esp := "Isso é apenas um teste"
+
+		comparaStrings(t, res, esp)
+	})
+
+	t.Run("Palavra desconhecida", func(t *testing.T) {
+		_, resultado := dicionario.Busca("desconhecido")
+		comparaErro(t, resultado, ErrNaoEncontrado)
+	})
+
+}
+
+func Test_Adiciona(t *testing.T) {
+	dicionario := Dicionario{}
+	dicionario.Adiciona("teste", "isso é apenas um teste")
+
+	esperado := "isso é apenas um teste"
+	result, err := dicionario.Busca("teste")
+	if err != nil {
+		t.Fatal(ErrNaoEncontrado)
+	}
+	if esperado != result {
+		t.Errorf("resultado %s, esperado %s", result, esperado)
+	}
+}
+
 func comparaStrings(t *testing.T, resultado, esperado string) {
 	t.Helper()
 	if resultado != esperado {
 		t.Errorf("Resultado %s, esperado %s dado %s", resultado, esperado, "test")
 	}
 }
-func Test_Busca(t *testing.T) {
-	dicionario := Dicionario{"Test": "Isso é apenas um teste"}
-	res := dicionario.Busca("Test")
-	esp := "Isso é apenas um teste"
+func comparaErro(t *testing.T, resultado, esperado error) {
+	t.Helper()
 
-	comparaStrings(t, res, esp)
+	if resultado != esperado {
+		t.Errorf("resultado erro '%s', esperado '%s'", resultado, esperado)
+	}
 }
