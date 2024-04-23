5fe02d3fc50c762d597c8678bc37aaae7ff6c3ca
Nesrux <joaomarcosdevs@gmail.com>
2024-04-22 21:33:08 -0300

    mocks

--- 09_mocks/mock.go
+++ 09_mocks/mock.go
@@ -7,19 +7,37 @@ import (
 	"time"
 )
 
-const (
-	ultimaPalavra  = "Go!"
-	inicioContagem = 3
-)
+// Sleeper te permite definir pausas
+type Sleeper interface {
+	Pausa()
+}
 
-func Contagem(saida io.Writer) {
+// SleeperConfiguravel é uma implementação de Sleepr com uma pausa definida
+type SleeperConfiguravel struct {
+	duracao time.Duration
+	pausa   func(time.Duration)
+}
+
+// Pausa vai pausar a execução pela Duração definida
+func (s *SleeperConfiguravel) Pausa() {
+	s.pausa(s.duracao)
+}
+
+const ultimaPalavra = "Vai!"
+const inicioContagem = 3
+
+// Contagem imprime uma contagem de 3 para a saída com um atraso determinado por um Sleeper
+func Contagem(saida io.Writer, sleeper Sleeper) {
 	for i := inicioContagem; i > 0; i-- {
-		time.Sleep(1 * time.Second)
-		fmt.Fprint(saida, i)
+		sleeper.Pausa()
+		fmt.Fprintln(saida, i)
 	}
+
+	sleeper.Pausa()
 	fmt.Fprint(saida, ultimaPalavra)
 }
 
 func main() {
-	Contagem(os.Stdout)
+	sleeper := &SleeperConfiguravel{1 * time.Second, time.Sleep}
+	Contagem(os.Stdout, sleeper)
 }
