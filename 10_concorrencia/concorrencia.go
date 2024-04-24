b570a1cae6afeada514e6cdf1b8109a8ba3f7c97
Nesrux <joaomarcosdevs@gmail.com>
2024-04-24 14:45:27 -0300

    concorrencia

--- 10_concorrencia/concorrencia.go
+++ 10_concorrencia/concorrencia.go
@@ -1,13 +1,24 @@
 package concorrencia
 
 type VerificadorWebsite func(string) bool
+type resultado struct {
+	string
+	bool
+}
 
 func VerificaWebsites(vw VerificadorWebsite, urls []string) map[string]bool {
-    resultados := make(map[string]bool)
+	resultados := make(map[string]bool)
+	canalResultado := make(chan resultado)
 
-    for _, url := range urls {
-        resultados[url] = vw(url)
-    }
+	for _, url := range urls {
+		go func(u string) {
+			canalResultado <- resultado{u, vw(u)}
+		}(url)
+	}
+	for i := 0; i < len(urls); i++ {
+		resultado := <-canalResultado
+		resultados[resultado.string] = resultado.bool
+	}
 
-    return resultados
+	return resultados
 }
--- 10_concorrencia/concorrencia_test.go
+++ 10_concorrencia/concorrencia_test.go
@@ -1,6 +1,7 @@
 package concorrencia
 
 import (
+	"reflect"
 	"testing"
 	"time"
 )
@@ -12,11 +13,40 @@ func slowStubVerificadorWebSite(_ string) bool {
 
 func Benchmark_verificaWebSites(b *testing.B) {
 	urls := make([]string, 100)
-	for i:=0; i < len(urls); i++{
+	for i := 0; i < len(urls); i++ {
 		urls[i] = "uma Url qualquer"
 	}
 
-	for i := 0; i <b.N; i++ {
+	for i := 0; i < b.N; i++ {
 		VerificaWebsites(slowStubVerificadorWebSite, urls)
+	}
+}
+
+/// aqui é do cara lá
+
+func mockVerificadorWebsite(url string) bool {
+	if url == "waat://furhurterwe.geds" {
+		return false
+	}
+	return true
+}
+
+func TestVerificaWebsites(t *testing.T) {
+	websites := []string{
+		"http://google.com",
+		"http://blog.gypsydave5.com",
+		"waat://furhurterwe.geds",
+	}
+
+	esperado := map[string]bool{
+		"http://google.com":          true,
+		"http://blog.gypsydave5.com": true,
+		"waat://furhurterwe.geds":    false,
+	}
+
+	resultado := VerificaWebsites(mockVerificadorWebsite, websites)
+
+	if !reflect.DeepEqual(esperado, resultado) {
+		t.Fatalf("esperado %v, resultado %v", esperado, resultado)
 	}
 }
